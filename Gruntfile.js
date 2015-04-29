1c7e87d7f28b15b227218cfd39f64d4d545ab1c7
PSmolic <psmolic@extensionengine.com>
2015-04-29 05:09:15 -0400

    added usmin

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,38 @@ module.exports = function(grunt) {
         }
       }
     },
+    copy:{
+      html: {
+        src: 'app/index.html', dest: 'dist/index.html'
+      },
+      files: {
+        expand: true,
+        dest: 'dist/partials',
+        cwd: 'app/partials/',
+        src: '*'
+      },
+      fonts: {
+        expand: true,
+        dest: 'dist/assets/fonts',
+        cwd: 'app/css/bootstrap/fonts/',
+        src: '*'
+      }
+    },
+    useminPrepare: {
+      options: {
+        dest: "dist"
+      },
+      html: 'app/index.html'
+    },
+    usemin: {
+      html:['dist/index.html'],
+      css: ['dist/**/*.css']
+    },
+    uglify: {
+      options: {
+        mangle: false
+      }
+    },
     watch: {
       files: ['<%= jshint.files %>'],
       tasks: ['jshint']
@@ -18,7 +50,13 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-usemin');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('build', ['copy:html', 'copy:files', 'copy:fonts', 'useminPrepare','concat','cssmin', 'uglify', 'usemin']);
 
 };
